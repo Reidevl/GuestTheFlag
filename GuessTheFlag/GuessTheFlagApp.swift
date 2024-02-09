b98a8f200c6060f5ab732595c9c38aa3e8e0210e
Reiwil Lugo <reiwillugo@gmail.com>
2024-02-09 09:47:17 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Reiwil Lugo on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
